d3eaa394489ef8d1ada6ee7fa93473480c075585
nnnsyyy <241374641@qq.com>
2017-09-28 17:06:04 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material' ;
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
