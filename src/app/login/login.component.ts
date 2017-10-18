76a506e062f015f6e917551a9c40f8bee949082c
nnnsyyy <241374641@qq.com>
2017-10-18 16:17:55 +0200

    build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,14 +7,14 @@ import { MdDialog, MdDialogRef } from '@angular/material';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    
-  user = {remember:false};
+
+  user = {username: '', password: '', remember:false};
 
   constructor(private dialogRef: MdDialogRef<LoginComponent>) { }
 
   ngOnInit() {
   }
-  
+
   onSubmit(){
       console.log("User: ", this.user);
       this.dialogRef.close();
